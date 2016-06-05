7380777669391075f43ad961cc795ca508fdb82a
Daniel D <cloudcrypt@hotmail.com>
2016-06-05 15:32:37 -0600

    Initial ShiftCarGrid function

--- a3/source/carMap.s
+++ a3/source/carMap.s
@@ -7,7 +7,7 @@ ClearCarGrid:
 
 .global	GenerateNewCars
 GenerateNewCars:
-	push	{r4-r10, lr}
+	push	{r4-r5, lr}
 	laneCtr	.req	r4
 	car	.req	r5
 	mov	laneCtr, #0
@@ -62,11 +62,130 @@ GenerateNewCars:
 
 	.unreq	laneCtr
 	.unreq	car
-	pop	{r4-r10, pc}
+	pop	{r4-r5, pc}
 
 // start shifting from bottom up
 .global	ShiftCarGrid
 ShiftCarGrid:
+	push	{r4-r10, lr}
+	row	.req	r4
+	lane 	.req	r5
+	car	.req	r6
+	vel	.req	r7
+	velCtr	.req	r8
+	len	.req	r9
+	lenCtr	.req	r10
+	mov 	row, #26
+
+	rowLoop:
+	mov 	lane, #0
+
+	laneLoop2:
+	mov	r0, lane
+	mov	r1, row
+	bl	GetCarCell
+	mov	car, r0
+	cmp	car, #0
+	beq	ignoreLane2
+
+	// vel
+	and	vel, car, #0b11
+	// velAlt
+	and	r1, car, #0b1100
+	lsr	r1, #2
+	// swap vel and velAlt in car
+	bic	car, #0b1111
+	lsl	r0, vel, #2
+	orr 	car, r0
+	orr	car, r1
+
+	// check vel for this shift cycle
+	cmp	vel, #0
+	beq	ignoreLane2
+
+	// clear car from grid
+	mov	r0, #0
+	mov	r1, lane
+	mov	r2, row
+	bl	SetCarCell
+	
+	// check if car is leaving rendered area of grid
+	add	r0, row, vel
+	cmp	r0, #26
+	bgt	ignoreLane2
+
+	// get car len
+	lsr	r0, car, #4
+	ldr	r1, =cars
+	ldr	r0, [r1, r0, lsl #2]
+	ldr	len, [r0]
+
+	// validate car's entire shift path for entire length of car
+	mov	velCtr, #1
+
+	velLoop:
+	mov	lenCtr, #0
+
+	lenLoop:
+	mov	r0, lane
+	add	r1, velCtr, lenCtr
+	add	r1, row
+	push	{r1}		// save potential victimCar row
+	bl	GetCarCell
+	pop	{r1}
+	cmp	r0, #0
+	bne	accidentHandler
+
+	add	lenCtr, #1
+	cmp	lenCtr, len
+	blt	lenLoop
+
+	add	velCtr, #1
+	cmp	velCtr, vel
+	ble	velLoop
+
+	// shift car by vel and set into grid
+	mov	r0, car
+	mov	r1, lane
+	add	r2, row, vel
+	bl	SetCarCell
+	b	ignoreLane2
+
+	accidentHandler:
+	// victimCar in r0, victimCar row in r1
+	mov	r2, r1
+	// set victimCar velocity to 0101 (1/1)
+	bic	r0, #0b1111
+	orr	r0, #0b0101
+	mov	r1, lane
+	push	{r2}		// save victimCar row
+	bl	SetCarCell
+	pop	{r2}
+
+	// get row above victimCar and shift car to row - car len
+	sub	r2, len
+	mov	r0, car
+	mov	r1, lane
+	bl	SetCarCell
+
+	ignoreLane2:
+	add	lane, #1
+	cmp	lane, #22
+	bne	laneLoop2
+
+	sub	row, #1
+	cmp	row, #-1
+	bne	rowLoop
+	.unreq	row
+	.unreq	lane
+	.unreq	car
+	.unreq	vel
+	.unreq	velCtr
+	.unreq	len
+	.unreq	lenCtr
+	pop	{r4-r10, pc}
+
+
 
 .global	SetCollisions
 UpdateCollisions:
@@ -134,6 +253,7 @@ CheckLane:
 
 
 .section .data
+// 22x27 Grid (topExt + cars)
 .global	carGrid
 carGrid:	
 	.rept	594
